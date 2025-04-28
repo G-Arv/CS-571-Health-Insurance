60d44c8451f3acc26bbb4a9ab9f45557897687dd
G-Arv <garvind@umass.edu>
2025-04-28 09:30:56 -0400

    Initial data parsing code for focus.js and changes to healthInsurance.csv

--- js/focus.js
+++ js/focus.js
@@ -1,2 +1,23 @@
 // This is where the code for the focus parts will be
 // TODO: fix the frontend to include this script from the start
+
+// Creates a pie chart for the given state
+// Parameters: state (string) - a given state
+function createPieChart(state) {
+    let color = d3.scaleOrdinal(["#264653", "#f4a261"]);
+    let stateData = null;
+
+    let data = d3.csv(`data/healthInsurance.csv`).then(output => {
+        for(let i = 0; i < output.length; ++i) {
+            if(output[i].State == state) {
+                stateData = output[i];
+                break;
+            }
+        }
+    });
+
+    console.log(stateData)
+
+}
+
+createPieChart("Massachusetts");
