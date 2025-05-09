9c007d92f867e8a682a50b576349acdaa51a81fa
Akshitaa Jay <akshijay13@gmail.com>
2025-05-09 01:18:50 -0400

    yes

--- js/focus.js
+++ js/focus.js
@@ -1,49 +1,55 @@
-// Creates the bar chart for Medicaid information for a given state
-// Parameters: state (string) - a given state
 function createBarChart2(state) {
-    let color = d3.scaleOrdinal(["hotpink", "dodgerblue"]);
+    let color = d3.scaleOrdinal(["hotpink", "royalblue"]);
     const data = d3.csv(`data/healthInsurance.csv`).then(output => {
         for(let i = 0; i < output.length; ++i) {
             let currState = output[i].State
             if(currState == state && currState != "Maine" && currState != "Connecticut") {
-                makeBar2(output[i])
+                makeBar(output[i])
                 break;
-            }
+            } 
         }
     });
 
     // Makes the bar chart specifically
     // Parameters: data (object) - the data for a given state
-    function makeBar2(data) {
+    function makeBar(data) {
         let barChart = d3.selectAll("#bar-chart2");
         const width = 500;
         const height = 500;
         const margin = { left: 60, bottom: 30, top: 30, right: 30 };
-        barChart.append("rect") .attr("width", height).attr("height", width).style("stroke", "none").style("fill", "lightblue");
+        barChart.append("rect").attr("width", height).attr("height", width).style("stroke", "none").style("fill", "lightblue");
         const barSearch = ["Medicaid_Enrollment_(2013)", "Medicaid_Enrollment_(2016)"];
         const barSearchCleaned = ["Medicaid Enrollment (2013)", "Medicaid Enrollment (2016)"];
         let data13 = parseInt(data[barSearch[0]]);
         let data16 = parseInt(data[barSearch[1]]);
+
         let scale = Math.pow(10, (Math.min(data13, data16).toString().length - 1));
         const barData = [Math.log(data13/scale), Math.log(data16/scale)];
+
         let max = Math.round(Math.max(...barData));
+
         let xScale = d3.scaleBand().domain(barSearchCleaned).range([margin.left, width - margin.right]);
+
         let yScale = d3.scaleLinear().domain([0, max]).range([height - margin.top, margin.bottom]);
+
         let xAxis = d3.axisBottom().tickValues(barSearchCleaned);
         xAxis.scale(xScale);
+
         let yAxis = d3.axisLeft().ticks(10);
         yAxis.scale(yScale);
+
         // Append axes to bar chart
         barChart.append("g").attr("transform", `translate(0, ${height - margin.bottom})`).call(xAxis);
         barChart.append("g").attr("transform", `translate(${margin.left}, 0)`).call(yAxis);
-        let barWidth = Math.floor((width - margin.left - margin.right)/4); // Create bars
+        let barWidth = Math.floor((width - margin.left - margin.right)/4);  // Create bars
+
         barChart.append('g').attr("class", "barChart").selectAll(".barChart").data(barData).enter().append("rect").style("fill", (d, i) => color(i))
                 .merge(barChart).attr("x", (d, i) => i * barWidth * 2 + margin.right * 3.75).attr("y", (d, i) => yScale(d)).attr("width", barWidth)
                 .attr("height", d => height - yScale(d) - margin.bottom).attr("opacity", 1);
         
         // Add label for y axis
-        barChart.append("text").attr("class", "yAxisLabel").attr("text-anchor", "end").attr("y", 5).attr("dy", "0.75em").attr("transform", "rotate(-90)")
-            .text("Natural Log of Patients Enrolled in Medicaid")
+        barChart.append("text").attr("class", "yAxisLabel").attr("text-anchor", "end").attr("y", 5).attr("dy", "0.75em")
+            .attr("transform", "rotate(-90)").text("Natural Log of Patients Enrolled in Medicaid")
 
     }
 }
